1060d07e907780943a1d36f8d36d3a5872192425
Harsh_313008 <91689978+Harsh313@users.noreply.github.com>
2024-08-01 12:45:51 +0530

    Delete C++ assignment/assignment 1 directory
    don't want anymore

--- C++ assignment/assignment 1/A3.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-	int a,b,c;
-	cin>>a>>b>>c;
-	cout<<"Sum is = "<<a+b+c<<"\nAverage is = "<<(a+b+c)/3;
-}
--- C++ assignment/assignment 1/A7.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-	int a,b;
-	cin>>a>>b;
-	cout<<"Addition "<<a+b<<"\nSubtraction "<<a-b<<"\nMultiplication "<<a*b<<"\nDivision "<<((float)a/b);
-	return 0;
-}
